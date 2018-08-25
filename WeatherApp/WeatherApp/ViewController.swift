dd4ff6b531e7487005029321fdf34981f417aa57
Stephen Walsh <sfwalshappdev@gmail.com>
2018-08-25 12:25:02 +0200

    proj init

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Stephen Walsh on 25/08/2018.
+//  Copyright © 2018 Stephen Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
